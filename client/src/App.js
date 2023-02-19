a4c6212649b372597b78db5bbc9ce919a93aa3df
lautixon <lautarobarrera574@gmail.com>
2023-02-18 21:03:29 -0300

    api rul

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ const NewsList = () => {
           setLoading(false);
           return;
         }
-        const response = await fetch(`https://vocenewsapi.vercel.app//news/${country}/${categorie}/${page}`);
+        const response = await fetch(`https://vocenewsapi.vercel.app/news/${country}/${categorie}/${page}`);
         const data = await response.json();
         const hasmore = Array.isArray(data) && data.length > 0;
         if (hasmore) {
